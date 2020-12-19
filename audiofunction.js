6c6331af0e31a2ed6c9e20f685f8111c80ab4c6d
yumyum1969 <75912713+yumyum1969@users.noreply.github.com>
2020-12-19 02:13:54 +0100

    add js, record function

---
+++ audiofunction.js
@@ -0,0 +1,89 @@
+var toggle = "off";
+
+pin.onclick = function(){
+  if(toggle == "off"){
+    pin.setAttribute("src","map.png");
+    toggle = "on";
+    testMusic.play();
+  }else {
+    pin.setAttribute
+    ("src","https://pngriver.com/wp-content/uploads/2018/04/Download-Pin-Map-Church-Logo-Png-Image-96327-For-Designing-Projects.png");
+    toggle = "off";
+    testMusic.pause();
+  }
+
+}
+pin2.onclick = function(){
+  if(toggle == "off"){
+    pin2.setAttribute("src","map.png");
+    toggle = "on";
+    rain1.play();
+  }else {
+    pin2.setAttribute("src","https://cdn.onlinewebfonts.com/svg/img_465535.png");
+    toggle = "off";
+    rain1.pause();
+  }
+
+}
+
+pin3.onclick = function(){
+  if(toggle == "off"){
+    pin3.setAttribute("src","map.png");
+    toggle = "on";
+    rain2.play();
+  }else {
+    pin3.setAttribute("src","https://i1.wp.com/www.gauss-friends.org/wp-content/uploads/2020/04/location-pin-connectsafely-37.png?w=800&ssl=1");
+    toggle = "off";
+    rain2.pause();
+  }
+
+}
+
+pin4.onclick = function(){
+  if(toggle == "off"){
+    pin4.setAttribute("src","map.png");
+    toggle = "on";
+    drill.play();
+  }else {
+    pin4.setAttribute("src","https://i1.wp.com/www.gauss-friends.org/wp-content/uploads/2020/04/location-pin-connectsafely-37.png?w=800&ssl=1");
+    toggle = "off";
+    drill.pause();
+  }
+
+}
+
+pin5.onclick = function(){
+  if(toggle == "off"){
+    pin5.setAttribute("src","map.png");
+    toggle = "on";
+    bell.play();
+  }else {
+    pin5.setAttribute("src","https://i1.wp.com/www.gauss-friends.org/wp-content/uploads/2020/04/location-pin-connectsafely-37.png?w=800&ssl=1");
+    toggle = "off";
+    bell.pause();
+  }
+  }
+pin6.onclick = function(){
+  if(toggle == "off"){
+    pin6.setAttribute("src","map.png");
+    toggle = "on";
+    people.play();
+  }else {
+    pin6.setAttribute("src","https://i1.wp.com/www.gauss-friends.org/wp-content/uploads/2020/04/location-pin-connectsafely-37.png?w=800&ssl=1");
+    toggle = "off";
+    people.pause();
+  }
+
+}
+pin7.onclick = function(){
+  if(toggle == "off"){
+    pin7.setAttribute("src","map.png");
+    toggle = "on";
+    trunk.play();
+  }else {
+    pin7.setAttribute("src","https://i1.wp.com/www.gauss-friends.org/wp-content/uploads/2020/04/location-pin-connectsafely-37.png?w=800&ssl=1");
+    toggle = "off";
+    trunk.pause();
+  }
+
+}
